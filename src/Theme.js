001a64d79cacb30a3c271764f2c4db1c4c1e8bf7
Jason <jason.ng7952@gmail.com>
2022-09-07 14:39:25 -0400

    Added global styling and theme properties

---
+++ src/Theme.js
@@ -0,0 +1,10 @@
+export const PrimaryTheme = {
+  colorPrimary: "#000",
+  colorSecondary: "#FFF",
+  colorAccent: "#F3E600",
+  colorBlue: "#04DAF6",
+  colorRed: "#FF003C",
+  fontWeight400: "400",
+  fontWeight700: "700",
+  fontFamily: "'Chakra Petch', sans-serif",
+}
