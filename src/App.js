8fbb75331989ea2f6f446c3a3f229026d5039933
Jason <jason.ng7952@gmail.com>
2022-09-07 14:37:59 -0400

    Added the layout of portfolio

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import React from 'react'
+import { GlobalStyle } from './Global';
+import { ThemeProvider } from 'styled-components';
+import { PrimaryTheme } from './Theme'
+import Nav from './components/Nav'
+import Home from './components/Home'
+import About from './components/About'
+import Project from './components/Project'
+import Contact from './components/Contact'
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={PrimaryTheme}>
+        <Nav />
+        <Home />
+        <About />
+        <Project />
+        <Contact />
+      </ThemeProvider>
+    </>
+  )
 }
 
-export default App;
+export default App
