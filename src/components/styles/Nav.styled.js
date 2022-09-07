235b9e0d46d31d0cedff4a725ac5066fe255b38c
Jason <jason.ng7952@gmail.com>
2022-09-07 16:49:21 -0400

    Changed color and padding

--- src/components/styles/Nav.styled.js
+++ src/components/styles/Nav.styled.js
@@ -1,15 +1,15 @@
 import styled from "styled-components";
 
-export const NavContainer = styled.div`
-  background-color: ${props => props.theme.colorPrimary};
+export const NavContainer = styled.header`
+  background-color: ${props => props.theme.colorAccent};
   font-family: ${props => props.theme.fontFamily};
-  padding: 1em;
+  padding: 0.2em 1em;
   display: flex;
   justify-content: space-between;
   align-items: center;
 `
 export const NavLogo = styled.div`
-  color: ${props => props.theme.colorSecondary};
+  color: ${props => props.theme.colorPrimary};
   font-weight: ${props => props.theme.fontWeight700};
   font-size: 2rem;
   cursor: pointer;
@@ -21,7 +21,7 @@ export const NavList = styled.ul`
 `
 
 export const NavLink = styled.li`
-  color: ${props => props.theme.colorSecondary};  
+  color: ${props => props.theme.colorPrimary};  
   font-weight: ${props => props.theme.fontWeight400};
   font-size: 1rem;
   margin: 0 1em;
