c0ba5f3f7b21d5ef78e411f70f798054c14019e9
Jason <jason.ng7952@gmail.com>
2022-09-08 19:34:12 -0400

    Added hamburger menu for smaller screens

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import { NavContainer, NavLogo, NavList, NavLink } from './styles/Nav.styled';
+import React, { useState } from 'react';
+import { NavContainer, NavLogo, NavList, NavLink, NavButton, NavHamburgerOpen, NavHamburgerClose } from './styles/Nav.styled';
 
 function Nav() {
+
+  const [click, setClick] = useState(false);
+
+  const displayNav = () => {
+    setClick(!click);
+  };
+
   return (
     <NavContainer>
       <NavLogo>JN</NavLogo>
-      <NavList>
-        <NavLink><a a href='#home'>Home</a></NavLink>
-        <NavLink><a a href='#about'>About</a></NavLink>
-        <NavLink><a a href='#projects'>Projects</a></NavLink>
-        <NavLink><a a href='#contact'>Contact</a></NavLink>
+      <NavList click={click} >
+        <NavLink click={click} onClick={displayNav}><a a href='#home'>Home</a></NavLink>
+        <NavLink click={click} onClick={displayNav}><a a href='#about'>About</a></NavLink>
+        <NavLink click={click} onClick={displayNav}><a a href='#projects'>Projects</a></NavLink>
+        <NavLink click={click} onClick={displayNav}><a a href='#contact'>Contact</a></NavLink>
       </NavList>
+      <NavButton onClick={displayNav}>
+        <NavHamburgerOpen click={click} />       
+        <NavHamburgerClose click={click} /> 
+      </NavButton>
     </NavContainer>
   )
 }
--- src/components/styles/Nav.styled.js
+++ src/components/styles/Nav.styled.js
@@ -23,6 +23,21 @@ export const NavLogo = styled.div`
 export const NavList = styled.ul`
   display: flex;
   list-style: none;
+
+  // When screen gets smaller, use hamburger button with menu.
+  @media (max-width: 768px) {
+    position: fixed;
+    background-color: ${props => props.click ? props.theme.colorPrimary : "none"};
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 100;
+
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+  }
 `
 
 export const NavLink = styled.li`
@@ -35,8 +50,96 @@ export const NavLink = styled.li`
     text-decoration: none;
   }
 
-  // Hide text links when screen gets smaller
+  // Hide text links when screen gets smaller, but if hamburger is clicked show the links.
   @media (max-width: 768px) {
+    display: ${props => props.click ? "block" : "none"};
+
+    a {
+      font-size: 2rem;
+    }
+  }
+`
+
+export const NavButton = styled.button`
+  padding: 0.5em;
+  background: transparent;
+  border: 0;
+  cursor: pointer;
+  z-index: 1000;
+
+  @media (min-width: 768px) {
     display: none;
+  }
+`
+export const NavHamburgerOpen = styled.span`
+  width: 2em;
+  height: 3px;
+  display: block;
+  position: relative;
+  background-color: ${props => props.theme.colorSecondary};
+
+  // On Click, remove the middle piece of the hamburger.
+  transition: ${props => props.click ? "all 0.5s ease-in-out" : "all 0.5s ease-in-out 0.3s"};
+  background: ${props => props.click ? "none" : ""};
+
+  &::before,
+  &::after {
+    content: "";
+    background-color: ${props => props.theme.colorSecondary};
+    height: 3px;
+    position: absolute;
+    left: 0;
+    right: 0;
+    // On Click, split the hamburger (top and bottom piece).
+    transition: ${props => props.click ? "all 0.5s ease-in-out" : "all 0.5s ease-in-out 0.3s"};
+  }
+
+  &::before {
+    width: 2em;
+    bottom: 6px;
+    // On Click, top piece goes right.
+    transform: ${props => props.click ? "translateX(10px)" : "translateX(0px)"};
+    background: ${props => props.click ? "none" : ""};
+  }
+
+  &::after {
+    width: 2em;
+    top: 6px;
+    // On Click, bottom piece goes left.
+    transform: ${props => props.click ? "translateX(-10px)" : "translateX(0px)"};
+    background: ${props => props.click ? "none" : ""};    
+  }
+`
+export const NavHamburgerClose = styled.span`
+  display: block;
+  position: relative;
+
+  &::before,
+  &::after {
+    content: "";
+    background-color: ${props => props.theme.colorSecondary};
+    height: 3px;
+    border-radius: 1em;
+    position: absolute;
+    left: 0;
+    right: 0;
+    // When the nav button is clicked, transition into a X symbol.
+    transition: ${props => props.click ? "all 0.5s ease-in-out 0.3s" : "all 0.5s ease-in-out"};
+  }
+
+  &::before {
+    width: 2em;
+    bottom: 6px;
+    // On Click, rotate top piece.
+    transform: ${props => props.click ? "translateY(6px) rotate(45deg) " : ""};
+    background: ${props => props.click ? "" : "none"};
+  }
+
+  &::after {
+    width: 2em;
+    top: 6px;
+    // On Click, rotate bottom piece
+    transform: ${props => props.click ? "translateY(-9px) rotate(-45deg) " : ""};
+    background: ${props => props.click ? "" : "none"};   
   }
 `
